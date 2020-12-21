c24acda596c38637311bc7c9d119d9eb908de21c
Monika <m.szymanska.macik@gmail.com>
2020-12-21 07:21:21 +0100

    displaying data on the home page

--- Tasker/Pages/Index.cshtml.cs
+++ Tasker/Pages/Index.cshtml.cs
@@ -9,8 +9,16 @@ namespace Tasker.Pages
 {
     public class IndexModel : PageModel
     {
+        public IEnumerable<Task> Tasks { get; set; }
+        private ApplicationDbContext _context;
+        public IndexModel(ApplicationDbContext context)
+        {
+            context = _context;
+        }
+
         public void OnGet()
         {
+            _context.Tasks.OrderBy(x => x.Priority);
         }
     }
 }
