8cfa5f99438a3c3ce3790447f561789b70579a4b
Monika <m.szymanska.macik@gmail.com>
2020-12-21 07:07:00 +0100

    registered the database context

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -9,6 +9,7 @@ namespace Tasker.Pages
 {
     public class CreateTaskModel : PageModel
     {
+        
         [BindProperty]
         public Task NewTask { get; set; }
         public void OnGet()
